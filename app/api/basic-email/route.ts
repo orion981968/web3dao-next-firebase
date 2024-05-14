5ccce56ce729849616c298a6c2cf860041c7d51a
orion981968 <jostob.o68@gmail.com>
2024-05-14 17:27:09 +0300

    update key

--- app/api/basic-email/route.ts
+++ app/api/basic-email/route.ts
@@ -3,22 +3,22 @@ const axios = require("axios");
 export async function POST(request: any) {
   const res = await request.json();
   const { to } = res;
-  const url = "https://us12.api.mailchimp.com/3.0/lists/91fc724c13";
+  // const url = "https://us12.api.mailchimp.com/3.0/lists/91fc724c13";
   const data = {
     members: [
       {
         email_address: to,
-        status: "subscribed",
-      },
-    ],
+        status: "subscribed"
+      }
+    ]
   };
 
   const jsonData = JSON.stringify(data);
   const options = {
     // method: "POST",
     headers: {
-      Authorization: "auth 31b3663472c0e9c255a8fc08d5b4aa3a-us12",
-    },
+      // Authorization: "auth 31b3663472c0e9c255a8fc08d5b4aa3a-us12"
+    }
   };
 
   const response = await axios.post(url, options, jsonData);
